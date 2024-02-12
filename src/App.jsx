f106b054d1cc0adbaf191c68a84c8be140c13280
Wies Munck <wies-wies@hotmail.com>
2024-02-12 15:16:29 +0100

    uninstall vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import './App.scss'
 
 function App() {
@@ -29,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-		<SpeedInsights />
     </>
   )
 }
