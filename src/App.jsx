b16363273c1aa83e6933945af3c36d3f32b0c1eb
Eager WIes <eagerwies@Wiess-MacBook-Pro-2.local>
2024-02-13 09:28:04 +0100

    working on sccs structutre

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './scss/main.scss'
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from '@vercel/analytics/react';
 
@@ -31,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-		< Analytics />
+		<Analytics />
 		<SpeedInsights />
     </>
   )
