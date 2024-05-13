e563d8f4a0c565db53da6274927cf21d4255f214
Wies Munck <wies-wies@hotmail.com>
2024-05-14 00:00:05 +0200

    fix the json import to work with a direct object innstead pog

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,27 @@
-
 import React, { useState, useEffect } from 'react';
-import HeroHome from "../components/HeroHome/HeroHome";
-import ViewProjects from "../components/ViewProjects/ViewProjects";
-import PreviewColumns from "../components/PreviewColumns/PreviewColumns";
-import TextImgSection from "../components/TextImgSection/TextImgSection";
-import TextOnlySection from "../components/TextOnlySection/TextOnlySection";
-import ReviewWrapper from "../components/Reviews/ReviewWrapper";
-import CtaSection from "../components/CtaSection/CtaSection";
+import HeroHome from '../components/HeroHome/HeroHome';
+import ViewProjects from '../components/ViewProjects/ViewProjects';
+import PreviewColumns from '../components/PreviewColumns/PreviewColumns';
+import TextImgSection from '../components/TextImgSection/TextImgSection';
+import TextOnlySection from '../components/TextOnlySection/TextOnlySection';
+import ReviewWrapper from '../components/Reviews/ReviewWrapper';
+import CtaSection from '../components/CtaSection/CtaSection';
+// Import data
+import HomeData from '../data/home/home.json';
 
 export default function HomePage() {
-	const [detailData, setDetailData] = useState();
-
-
-	useEffect(() => {
-		//
-		import(`../data/home/home.json`)
-			 .then((data) => {
-
-				  setDetailData(data);
-			 })
-			 .catch((error) => {
-				console.log(error)
-			 });
-  }, []);
+	// Set data
+	const detailData = HomeData;
 
 	return (
 		<>
-			<HeroHome key="1" data={detailData}/>
-			<ViewProjects key="2"/>
-			<PreviewColumns key="3"/>
-			<TextImgSection key="4"/>
-			<ReviewWrapper key="5"/>
-			<TextOnlySection key="6"/>
-			<CtaSection key="7"/>
+			<HeroHome key="1" data={detailData.HeroHome} />
+			<ViewProjects key="2" />
+			<PreviewColumns key="3" />
+			<TextImgSection key="4" />
+			<ReviewWrapper key="5" />
+			<TextOnlySection key="6" />
+			<CtaSection key="7" />
 		</>
-	)
+	);
 }
