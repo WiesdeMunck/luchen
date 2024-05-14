51ec5c2cb4ac6cc390422ac254a16a5fe785da05
Wies Munck <wies-wies@hotmail.com>
2024-05-14 08:06:48 +0200

    finished the hero's on home dynamic data

--- src/components/HeroHome/HeroHome.jsx
+++ src/components/HeroHome/HeroHome.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import MainBtn from '../buttons/MainBtn';
-import HeaderOne from '../Typography/HeaderOne';
 
 export default function HeroHome({data}) {
+
+	// Why was I Doing this?
   useEffect(() => {
     const portraitImg = new Image();
-    portraitImg.src = '/img/self-portrait.webp';
+    portraitImg.src = data.imgPerson;
 
     const backgroundImg = new Image();
-    backgroundImg.src = '/img/home-being-plasterd.webp';
+    backgroundImg.src = data.imgBackground;
   }, []);
 
   console.log(data)
@@ -17,17 +18,17 @@ export default function HeroHome({data}) {
     <section className="hero-home">
       <div className="hero-home__images-wrapper">
         <div className="hero-home__fg-image-wrapper">
-          <img src="/img/self-portrait.webp" alt="" className="hero-home__fg-image" />
+          <img src={data.imgPerson} alt="" className="hero-home__fg-image" />
         </div>
         <div className="hero-home__bg-image-wrapper">
-          <img src="/img/home-being-plasterd.webp" alt="" className="hero-home__bg-image" />
+          <img src={data.imgBackground} alt="" className="hero-home__bg-image" />
         </div>
       </div>
       <div className="hero-home__content">
-        <HeaderOne classNames="hero-home__title" content="Expert plastering, exceptional results" />
+        <h1 classNames="hero-home__title">{data.title}</h1>
         <div className="hero-home__button-wrapper">
-          <MainBtn link={'/contact'} content="Huur mij in!" />
-          <MainBtn link={'/contact'} classNames={'btn--secondary'} content="contact me" />
+          <MainBtn link={data.primaryBtnLink} content={data.primaryBtnContent} />
+          <MainBtn link={data.secondaryBtnLink} classNames={'btn--secondary'} content={data.secondaryBtnContent} />
         </div>
       </div>
     </section>
--- src/components/Typography/HeaderOne.jsx
+++ src/components/Typography/HeaderOne.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// This component is never needed we handle styling in the global css or typography css
 export default function HeaderOne({content, classNames}) {
 	return (
 		<h1 id="mainContent" className={classNames && classNames} >{content}</h1>
